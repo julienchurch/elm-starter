a798bbeb8b26bd1f9474486d108c29a43cbd70de
Julien Church <julienchurch@gmail.com>
2016-09-07 03:35:33 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname + "/build"),
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(css|sass)$/,
+        loaders: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node-modules/],
+        loader: "elm-webpack"
+      }
+    ],
+    noParse: /\.elm$/,
+  }
+}
