137c693106609a322f8b966dc75be5e9089edd56
Julien Church <julienchurch@gmail.com>
2016-10-11 21:21:16 -0700

    Flesh out using Html.App.program
    Previously `main` was simply `text "<text value>"` for simplicity. This proved
    annoying to start off with when I typically use `Html.App.program` anyway.

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,37 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, text)
+import Html.App as App
 
-main = text "An Elm starter app."
+type alias Model =
+  ()
+
+type Msg
+  = NoOp
+
+init : (Model, Cmd Msg)
+init =
+  () ! []
+
+view : Model -> Html Msg
+view model =
+  text "An Elm starter app."
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    _ -> () ! []
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+main : Program Never
+main =
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
